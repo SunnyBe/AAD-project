dd9e90eb446a22b334c53a3cad339d94492bcddc
Zistus Ndu <sunday@ajocard.com>
2020-08-29 21:14:12 +0100

    First commit

---
+++ app/src/main/java/com/sunday/aadproject/splash/SplashActivity.kt
@@ -0,0 +1,6 @@
+package com.sunday.aadproject.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import com.sunday.aadproject.R
+
+class SplashActivity: AppCompatActivity(R.layout.activity_splash)
